cfab943daa93f01e2cf292a2a079b1010ac8071f
Olaolu Ajose <115143085+olaolu-tws@users.noreply.github.com>
2023-05-25 21:42:35 +0200

    #199 Web Service Dependencies, FileUpload Controller. Author:  Rosa  & Olaolu

--- blopup.fileupload.module/api/src/main/java/org/openmrs/module/blopup/fileupload/module/api/impl/BlopupfileuploadmoduleServiceImpl.java
+++ blopup.fileupload.module/api/src/main/java/org/openmrs/module/blopup/fileupload/module/api/impl/BlopupfileuploadmoduleServiceImpl.java
@@ -3,7 +3,7 @@
  * v. 2.0. If a copy of the MPL was not distributed with this file, You can
  * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
  * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
- *
+ * <p>
  * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
  * graphic logo is a trademark of OpenMRS Inc.
  */
@@ -15,38 +15,89 @@ import org.openmrs.api.impl.BaseOpenmrsService;
 import org.openmrs.module.blopup.fileupload.module.Item;
 import org.openmrs.module.blopup.fileupload.module.api.BlopupfileuploadmoduleService;
 import org.openmrs.module.blopup.fileupload.module.api.dao.BlopupfileuploadmoduleDao;
+import org.openmrs.module.blopup.fileupload.module.api.exceptions.StorageException;
+import org.openmrs.module.blopup.fileupload.module.api.models.StorageProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Objects;
 
 public class BlopupfileuploadmoduleServiceImpl extends BaseOpenmrsService implements BlopupfileuploadmoduleService {
-	
-	BlopupfileuploadmoduleDao dao;
-	
-	UserService userService;
-	
-	/**
-	 * Injected in moduleApplicationContext.xml
-	 */
-	public void setDao(BlopupfileuploadmoduleDao dao) {
-		this.dao = dao;
-	}
-	
-	/**
-	 * Injected in moduleApplicationContext.xml
-	 */
-	public void setUserService(UserService userService) {
-		this.userService = userService;
-	}
-	
-	@Override
-	public Item getItemByUuid(String uuid) throws APIException {
-		return dao.getItemByUuid(uuid);
-	}
-	
-	@Override
-	public Item saveItem(Item item) throws APIException {
-		if (item.getOwner() == null) {
-			item.setOwner(userService.getUser(1));
-		}
-		
-		return dao.saveItem(item);
-	}
+
+    BlopupfileuploadmoduleDao dao;
+
+    UserService userService;
+
+    private final Path rootLocation;
+
+    @Autowired
+    public BlopupfileuploadmoduleServiceImpl(StorageProperties properties) {
+        this.rootLocation = properties == null ? Paths.get("/legal_consent") : Paths.get(properties.getLocation());
+    }
+
+    @Override
+    public void store(MultipartFile file) {
+        try {
+            init();
+
+            Path destinationFile = this.rootLocation.resolve(
+                            Paths.get(Objects.requireNonNull(file.getOriginalFilename())))
+                    .normalize().toAbsolutePath();
+            if (!destinationFile.getParent().equals(this.rootLocation.toAbsolutePath())) {
+                // This is a security check
+                throw new StorageException(
+                        "Cannot store file outside current directory.");
+            }
+            try (InputStream inputStream = file.getInputStream()) {
+                Files.copy(inputStream, destinationFile,
+                        StandardCopyOption.REPLACE_EXISTING);
+            }
+        } catch (IOException e) {
+            throw new StorageException("Failed to store file.", e);
+        }
+    }
+
+    @Override
+    public void init() {
+        try {
+            Files.createDirectories(rootLocation);
+        } catch (IOException e) {
+            throw new StorageException("Could not initialize storage", e);
+        }
+    }
+
+    /**
+     * Injected in moduleApplicationContext.xml
+     */
+    public void setDao(BlopupfileuploadmoduleDao dao) {
+        this.dao = dao;
+    }
+
+    /**
+     * Injected in moduleApplicationContext.xml
+     */
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public Item getItemByUuid(String uuid) throws APIException {
+        return dao.getItemByUuid(uuid);
+    }
+
+    @Override
+    public Item saveItem(Item item) throws APIException {
+        if (item.getOwner() == null) {
+            item.setOwner(userService.getUser(1));
+        }
+
+        return dao.saveItem(item);
+    }
 }
---
+++ blopup.fileupload.module/omod/src/test/java/org/openmrs/module/blopup/fileupload/module/BlopupfileuploadmoduleServiceTest.java
@@ -0,0 +1,61 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ * <p>
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.module.blopup.fileupload.module;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.openmrs.module.blopup.fileupload.module.api.exceptions.StorageException;
+import org.openmrs.module.blopup.fileupload.module.api.impl.BlopupfileuploadmoduleServiceImpl;
+import org.openmrs.module.blopup.fileupload.module.web.controller.FileUploadController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockMultipartFile;
+
+import java.util.Objects;
+
+/**
+ * This is a unit test, which verifies logic in BlopupfileuploadmoduleService. It doesn't extend
+ * BaseModuleContextSensitiveTest, thus it is run without the in-memory DB and Spring context.
+ */
+public class BlopupfileuploadmoduleServiceTest {
+
+    @Mock
+    BlopupfileuploadmoduleServiceImpl basicModuleService;
+
+    @InjectMocks
+    FileUploadController fileUploadController;
+
+
+    @Before
+    public void setupMocks() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void shouldSaveUploadedFile() {
+        MockMultipartFile multipartFile = new MockMultipartFile("file", "test.txt",
+                "text/plain", "Spring Framework".getBytes());
+
+        ResponseEntity response = fileUploadController.handleFileUpload(multipartFile);
+        assert (response.getStatusCode().equals(HttpStatus.OK));
+        assert (Objects.equals(response.getBody(), "You successfully uploaded test.txt!"));
+
+    }
+
+    @Test(expected = StorageException.class)
+    public void shouldThrowStorageExceptionIfFileIsEmpty() {
+
+        fileUploadController.handleFileUpload(null);
+
+    }
+}
