06d7263c7fb3390cffc68a4c3bba704a8386f691
Alejandro Batanero <alejandro.batanero@thoughtworks.com>
2024-01-19 12:25:02 +0100

    Add Treatment encounter type

---
+++ api/src/main/resources/sql/2024-01-19_add_treatment_encounter_type.sql
@@ -0,0 +1,3 @@
+INSERT INTO `encounter_type` (`encounter_type_id`, `name`, `description`, `creator`, `date_created`, `uuid`)
+VALUES
+	(9, 'Treatment', 'Encounter type to capture treatments recommended due to high blood pressure', 1, NOW(), '29d9512b-69aa-47bf-ae16-fb73bb48748a');
