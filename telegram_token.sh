da85eeb77a1ea2d50955a9f92a57583e6372676c
rosapalli <rosa.palli@thoughtworks.com>
2024-01-26 11:03:15 +0100

    BLOPUP-355: rename properties file to application.properties

--- telegram_token.sh
+++ telegram_token.sh
@@ -2,4 +2,4 @@
 
 TOKEN=$1
 
-sed -i'' -e "s/{TELEGRAM_BOT_TOKEN}/$TOKEN/g" api/src/main/resources/telegram.properties
+sed -i'' -e "s/{TELEGRAM_BOT_TOKEN}/$TOKEN/g" api/src/main/resources/application.properties
