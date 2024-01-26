e0068e62879e5e1f6e18d875d57f9c5013fa4ac4
rosapalli <rosa.palli@thoughtworks.com>
2024-01-26 11:20:53 +0100

    BLOPUP-355: add encounter role and provider attribute type liquibase migrations

---
+++ api/src/main/resources/sql/2024-01-26_add_registered_doctor_encounter_role.sql
@@ -0,0 +1,3 @@
+INSERT INTO `encounter_role` (`encounter_role_id`, `name`, `description`, `creator`, `date_created`, `uuid`)
+VALUES
+    (10, 'Registered Doctor', 'A registered doctor who can recommend a treatment and prescribe medication to a patient', 1, NOW(), 'b09b056d-9eaf-41c9-a420-e2303e8f1c96');
---
+++ api/src/main/resources/sql/2024-01-26_add_telegram_chat_id_provider_attribute_type.sql
@@ -0,0 +1,3 @@
+INSERT INTO `provider_attribute_type` (`provider_attribute_type_id`, `name`, `description`, `datatype`, `creator`, `date_created`, `uuid`, `min_occurs`)
+VALUES
+    (1, 'Telegram Chat Id', 'The Provider Telegram chat id with the Blopup_bot', 'org.openmrs.customdatatype.datatype.FreeTextDatatype', 1, NOW(), '8d87236c-c2cc-11de-8d13-0010c6dffd0f',0);
