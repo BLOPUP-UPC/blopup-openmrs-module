7f35ce1a014d62f7bd8b7532da3d938696454f12
Carolina Acosta <carolina.acosta@thoughtworks.com>
2024-03-18 12:28:54 +0100

    BLOPUP-391 add registration number as provider attribute type liquibase migration

---
+++ api/src/main/resources/sql/2024-03-18_add_register_number_provider_attribute_type.sql
@@ -0,0 +1,3 @@
+INSERT INTO `provider_attribute_type` (`provider_attribute_type_id`, `name`, `description`, `datatype`, `creator`, `date_created`, `uuid`, `min_occurs`)
+VALUES
+    (1, 'Registration Number', 'registration number of the doctor.', 'org.openmrs.customdatatype.datatype.FreeTextDatatype', 1, NOW(), 'f4778183-b518-4842-946a-794fbff0e2e1',0);
